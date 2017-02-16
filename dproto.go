dbff99a3951a796ce2cd1316b8d154e9f7bf4bbe
Craig Hesling <craig@hesling.com>
2017-02-16 02:49:43 -0500

    Start of higher level abstracted dproto interface

--- dproto.go
+++ dproto.go
@@ -19,10 +19,93 @@
 // https://developers.google.com/protocol-buffers/docs/encoding
 package dproto
 
-// Field represents Protobuf's field numbers
+import (
+	"fmt"
+
+	"os"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+)
+
+// FieldNum represents Protobuf's field numbers
 type FieldNum uint32
 
-// High Level Combiner Interface Goes Here
+// ProtoFieldMap associates field numbers with it's high-level Protobuf type.
+type ProtoFieldMap struct {
+	field2type map[FieldNum]descriptor.FieldDescriptorProto_Type
+	type2field map[descriptor.FieldDescriptorProto_Type]FieldNum
+}
+
+// NewProtoFieldMap create a new ProtoFieldMap object.
+func NewProtoFieldMap() *ProtoFieldMap {
+	var m = new(ProtoFieldMap)
+	m.Reset()
+	return m
+}
+
+// Reset clears the stored associations inside a ProtoFieldMap
+func (m *ProtoFieldMap) Reset() {
+	m.field2type = make(map[FieldNum]descriptor.FieldDescriptorProto_Type)
+	m.type2field = make(map[descriptor.FieldDescriptorProto_Type]FieldNum)
+}
+
+// Add adds a Field-Type association to a ProtoFieldMap
+func (m *ProtoFieldMap) Add(field FieldNum, typ descriptor.FieldDescriptorProto_Type) {
+	m.field2type[field] = typ
+	m.type2field[typ] = field
+
+	if len(m.field2type) != len(m.type2field) {
+		fmt.Fprintf(os.Stderr, "Error - ProtoTypeMap is inconsistent")
+	}
+}
+
+// RemoveByField removes the Field-Type association from a ProtoFieldMap
+// that has the specified field number.
+// It returns true if the association was found and removed, false otherwise
+func (m *ProtoFieldMap) RemoveByField(field FieldNum) (ok bool) {
+	if typ, ok := m.field2type[field]; ok {
+		delete(m.field2type, field)
+		delete(m.type2field, typ)
+	}
+	return
+}
+
+// RemoveByType removes the Field-Type association from a ProtoFieldMap
+// that has the specified type.
+// It returns true if the association was found and removed, false otherwise
+func (m *ProtoFieldMap) RemoveByType(typ descriptor.FieldDescriptorProto_Type) (ok bool) {
+	if field, ok := m.type2field[typ]; ok {
+		delete(m.type2field, typ)
+		delete(m.field2type, field)
+	}
+	return
+}
+
+// Print shows the ProtoFieldMap to the user for debugging purposes.
+func (m *ProtoFieldMap) Print() {
+	fmt.Println(m)
+}
+
+var protoType2WireType = map[descriptor.FieldDescriptorProto_Type]WireType{
+	descriptor.FieldDescriptorProto_TYPE_DOUBLE:  proto.WireFixed64,
+	descriptor.FieldDescriptorProto_TYPE_FLOAT:   proto.WireFixed32,
+	descriptor.FieldDescriptorProto_TYPE_INT64:   proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_UINT64:  proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_INT32:   proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_UINT32:  proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_FIXED64: proto.WireFixed64,
+	descriptor.FieldDescriptorProto_TYPE_FIXED32: proto.WireFixed32,
+	descriptor.FieldDescriptorProto_TYPE_BOOL:    proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_STRING:  proto.WireBytes,
+	// descriptor.FieldDescriptorProto_TYPE_GROUP: proto.WireStartGroup
+	descriptor.FieldDescriptorProto_TYPE_MESSAGE:  proto.WireBytes,
+	descriptor.FieldDescriptorProto_TYPE_ENUM:     proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_SFIXED32: proto.WireFixed32,
+	descriptor.FieldDescriptorProto_TYPE_SFIXED64: proto.WireFixed64,
+	descriptor.FieldDescriptorProto_TYPE_SINT32:   proto.WireVarint,
+	descriptor.FieldDescriptorProto_TYPE_SINT64:   proto.WireVarint,
+}
 
 // Unmarshal will unmarshal a byte array into a WireMessage
 func Unmarshal(buf []byte) (*WireMessage, error) {
